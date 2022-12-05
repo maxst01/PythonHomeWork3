dd1735811165c16d62e08fdce8fd434aeedeef26
Assauit <maxst01@list.ru>
2022-12-05 08:06:15 +0700

    DZ1

---
+++ PythonHomeWork3.py
@@ -0,0 +1,11 @@
+# lst = []
+# lst = list(map(int, input("Введите числа через пробел:\n").split()))
+# def sum_odd_index(lst):
+#     s = 0
+#     for i in range(len(lst)):
+#         if i % 2 != 0:
+#             s += lst[i]
+#     print(f"Сумма равна: {s}")
+# sum_odd_index(lst)
+
+
